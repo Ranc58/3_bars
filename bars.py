0eb7d102901b803704a029976131350acaffd53d
Ranc58 <rancvova@gmail.com>
2017-05-11 16:49:00 +0300

    Getting seats

--- bars.py
+++ bars.py
@@ -1,8 +1,16 @@
 import json
+import os
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        print('file not exist')
+    with open(filepath, 'r', encoding='windows-1251') as file_handler:
+        content_json = json.load(file_handler)
+        # print(type(content_json))
+        #print(content_json[1])
+        for seats in content_json:
+            print ('%s -> %s' %(seats['Name'], seats['SeatsCount']))
 
 
 def get_biggest_bar(data):
@@ -19,3 +27,6 @@ def get_closest_bar(data, longitude, latitude):
 
 if __name__ == '__main__':
     pass
+
+input_way = 'd:\pycharmprj\\3_bars\\bars.json'
+load_data(input_way)
