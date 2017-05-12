76b3fa36939bea96488470db2f77fc0d2e14da64
Ranc58 <rancvova@gmail.com>
2017-05-12 16:59:29 +0300

    Getting seats

--- bars.py
+++ bars.py
@@ -1,25 +1,27 @@
 import json
-import os
 
 
 def load_data(func):
     def wrapped(filepath):
-        if not os.path.exists(filepath):
-            print('file not exist')
-        with open(filepath, 'r', encoding='windows-1251') as file_handler:
+        try:
+            file_handler = open(filepath, 'r', encoding='windows-1251')
             content_json = json.load(file_handler)
-            sorted_by_seats = sorted(content_json,
-                                     key=lambda seats: seats['SeatsCount'])
-            func(sorted_by_seats)
-
+            func(content_json)
+            file_handler.close()
+        except ValueError:
+            print('JSON file contain errors! ')
+        except FileNotFoundError:
+            print('Not found JSON file. Please check way. ')
     return wrapped
 
 
 @load_data
-def get_biggest_bar(sorted_by_seats):
+def get_biggest_bar(content_json):
+    sorted_by_seats = sorted(content_json,
+                             key=lambda seats: seats['SeatsCount'])
     result = sorted_by_seats.pop()
     for bar in sorted_by_seats:
-        print(bar['geoData']['coordinates'])  # TODO Удалить!
+        # print(bar['geoData']['coordinates'])  # TODO Удалить!
         if bar['SeatsCount'] == result['SeatsCount']:
             print('%s имеет %s мест в зале. '
                   'Это один из самых больших баров в списке.' % (
@@ -30,7 +32,9 @@ def get_biggest_bar(sorted_by_seats):
 
 
 @load_data
-def get_smallest_bar(sorted_by_seats):
+def get_smallest_bar(content_json):
+    sorted_by_seats = sorted(content_json,
+                             key=lambda seats: seats['SeatsCount'])
     result = sorted_by_seats.pop(0)
     for bar in sorted_by_seats:
         if bar['SeatsCount'] == result['SeatsCount']:
@@ -43,19 +47,23 @@ def get_smallest_bar(sorted_by_seats):
 
 
 @load_data
-def get_closest_bar(sorted_by_seats):  # , longitude, latitude):
-    my_coord=[38, 57]
-    list_of_coords=[]
+def get_closest_bar(content_json):  # , longitude, latitude):
+    my_coord = [38, 57]
+    list_of_coords = []
+    sorted_by_seats = sorted(content_json,
+                             key=lambda seats: seats['SeatsCount'])
     for bar in sorted_by_seats:
-        #list_of_coords.append(my_coord-bar['geoData']['coordinates'])
-        list_of_coords.append([(my_coord[0]-bar['geoData']['coordinates'][0]),(my_coord[1]-bar['geoData']['coordinates'][1])])
+        # list_of_coords.append(my_coord-bar['geoData']  TODO Удалить!
+        #                       ['coordinates'])         TODO Удалить!
+        list_of_coords.append([(my_coord[0] -
+                                bar['geoData']['coordinates'][0]),
+                               (my_coord[1] -
+                                bar['geoData']['coordinates'][1])])
     print(sorted(list_of_coords))
 
 
 if __name__ == '__main__':
-    pass
-
-input_way = 'D:\prj\\3_bars\\bars.json'
-#get_biggest_bar(input_way)
-#get_smallest_bar(input_way)
-get_closest_bar(input_way)
+    input_way = 'D:\Pycharmprj\\3_bars\\bars.json'
+    # get_biggest_bar(input_way)
+    # get_smallest_bar(input_way)
+    get_closest_bar(input_way)
