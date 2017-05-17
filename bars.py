22f3cc982a94178005be6cf712b343556641c394
Ranc58 <rancvova@gmail.com>
2017-05-17 16:37:06 +0300

    Need repair closest bar

--- bars.py
+++ bars.py
@@ -1,32 +1,54 @@
 import json
-import os
+import sys
 
 
 def load_data(func):
-    def wrapped(filepath):
-        if not os.path.exists(filepath):
-            print('file not exist')
-        with open(filepath, 'r', encoding='windows-1251') as file_handler:
-            content_json = json.load(file_handler)
-            sorted_by_seats = sorted(content_json,
-                                     key=lambda seats: seats['SeatsCount'])
-            func(sorted_by_seats)
-
+    def wrapped(filepath, *args):
+        try:
+            with open(filepath, 'r') as file_handler:
+                content_json = json.load(file_handler)
+                sorted_by_seats = sorted(content_json,
+                                        key = lambda seats: seats['SeatsCount'])
+                func(sorted_by_seats, *args)
+        except Exception as error:
+            print('\n',error,'\n Error! Check JSON file and directory way\n')
+            select_action_to_do()
     return wrapped
+
+
+def select_action_to_do():
+    select_action = input('Please select what you want to do: '
+                          '\n 1)Find smallest bar. '
+                          '\n 2)Find biggest bar. '
+                          '\n 3)Find closest bar'
+                          '\n Or type "exit" to exit: ')
+    if select_action != 'exit':
+        input_way='d:/pycharmprj/3_bars/bars.json'
+        #input_way = input('Please enter way to JSON file: ')
+        if select_action == '1':
+            get_smallest_bar(input_way)
+        elif select_action == '2':
+            get_biggest_bar(input_way)
+        elif select_action == '3':
+            longitude = input('Please enter longitude: ')
+            latitude = input('Please enter latitude: ')
+            get_closest_bar(input_way, float(longitude), float(latitude), )
+    else:
+        print('GoodBye!')
+        sys.exit()
 
 
 @load_data
 def get_biggest_bar(sorted_by_seats):
     result = sorted_by_seats.pop()
     for bar in sorted_by_seats:
-       # print(bar['geoData']['coordinates'])  # TODO Удалить!
         if bar['SeatsCount'] == result['SeatsCount']:
             print('%s имеет %s мест в зале. '
-                  'Это один из самых больших баров в списке.' % (
-                      bar['Name'], bar['SeatsCount']))
+                'Это один из самых больших баров в списке.' % (
+                    bar['Name'], bar['SeatsCount']))
     else:
         print('%s имеет %s мест в зале. Это самый большой бар в списке.'
-              % (result['Name'], result['SeatsCount']))
+            % (result['Name'], result['SeatsCount']))
 
 
 @load_data
@@ -35,43 +57,32 @@ def get_smallest_bar(sorted_by_seats):
     for bar in sorted_by_seats:
         if bar['SeatsCount'] == result['SeatsCount']:
             print('%s имеет %s мест в зале. '
-                  'Это один из самых маленьких баров в списке.' % (
-                      bar['Name'], bar['SeatsCount']))
+                'Это один из самых маленьких баров в списке.' % (
+                    bar['Name'], bar['SeatsCount']))
     else:
         print('%s имеет %s мест в зале. Это самый маленький бар в списке.'
-              % (result['Name'], result['SeatsCount']))
+            % (result['Name'], result['SeatsCount']))
 
 
 @load_data
-def get_closest_bar(sorted_by_seats):  # , longitude, latitude):
-    my_coord=[[37, 53]]
-    list_of_coords=[]
+def get_closest_bar(sorted_by_seats ,longitude, latitude):
+    my_coord = [[longitude, latitude]]
+    list_of_coords = []
     for bar in sorted_by_seats:
-        #list_of_coords.append(my_coord-bar['geoData']['coordinates'])
-        #value=my_coord[0][0]-bar['geoData']['coordinates'][0],my_coord[0][1]-bar['geoData']['coordinates'][1]
-        #list_of_coords.update({bar['Name']:value})
-        list_of_coords.append([(my_coord[0][0] - bar['geoData']['coordinates'][0]),(my_coord[0][1] - bar['geoData']['coordinates'][1])])
+        list_of_coords.append([(bar['geoData']['coordinates'][0]-my_coord[0][0]),(bar['geoData']['coordinates'][1]-my_coord[0][1])])
         list_of_coords.append(bar['Name'])
-        #for small_a,small_b in list_of_coords
-        #    print(min(small_a, small_b))
-        #list_of_coords.append([bar['geoData']['coordinates'][0], bar['geoData']['coordinates'][1]])
-        #for bar in list_of_coords:
-        #    print(bar[1],bar[2])
-    #for bars in list_of_coords:
-    #    for bar in bars[1:3]:
-    #        print(bar)
+        list_of_coords.append(bar['Address'])
     print(list_of_coords)
-    s=min(list_of_coords[::2])
-    print(s)
-    print(list_of_coords.index(s))
-    print(list_of_coords.pop(205))
-
+    nearest_bar_coord = min(list_of_coords[::3])
+    print(nearest_bar_coord)
+    positiion_in_bar_coord_list = list_of_coords.index(nearest_bar_coord)
+    print("Бар %s находится ближе всего к вам. Адрес: %s" % (list_of_coords.pop(positiion_in_bar_coord_list + 1), list_of_coords.pop(positiion_in_bar_coord_list + 1)))
 
 
 if __name__ == '__main__':
-    pass
-
-input_way = 'D:\prj\\3_bars\\bars.json'
-#get_biggest_bar(input_way)
-#get_smallest_bar(input_way)
-get_closest_bar(input_way)
+    #select_action_to_do()
+    #get_closest_bar('D:\\PyCharmPrj\\3_bars\\bars.json',37.464747458690351,55.784996653238541)
+    N = int(input())
+    lst = map(int, (input().split()))
+    x = int(input())
+    print(min(lst, key=lambda a: abs(a - x)))
