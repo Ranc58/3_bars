098ccf51c1eacae29ba2806bce57ab2661c54603
Ranc58 <rancvova@gmail.com>
2017-05-18 10:02:18 +0300

    some small refactor

--- bars.py
+++ bars.py
@@ -1,5 +1,5 @@
+import os
 import json
-import sys
 import math
 
 
@@ -11,37 +11,9 @@ def load_data(func):
                 sorted_by_seats = sorted(content_json,
                                          key=lambda seats: seats['SeatsCount'])
                 func(sorted_by_seats, *args)
-        except Exception as error:
-            print('\n', error, '\n Error! Check JSON file and directory way\n')
-            select_action_to_do()
+        except ValueError as error:
+            print('Error!Check JSON file content!\n', error)
     return wrapped
-
-
-def select_action_to_do():
-    input_way = input('Please enter way to JSON file: ')
-    select_action = input('Please select what you want to do: '
-                          '\n 1)Find smallest bar. '
-                          '\n 2)Find biggest bar. '
-                          '\n 3)Find closest bar'
-                          '\n Or type "exit" to exit: ')
-    if select_action == '1':
-        get_smallest_bar(input_way)
-    elif select_action == '2':
-        get_biggest_bar(input_way)
-    elif select_action == '3':
-        try:
-            longitude = input('Please enter longitude: ')
-            latitude = input('Please enter latitude: ')
-            get_closest_bar(input_way, float(longitude), float(latitude))
-        except ValueError:
-            print('Incorrect longitude and latitude! Enter correct values.')
-            select_action_to_do()
-    elif select_action == 'exit':
-        print('GoodBye!')
-        sys.exit()
-    else:
-        print("Error! Type correct variant!")
-        select_action_to_do()
 
 
 @load_data
@@ -66,22 +38,49 @@ def get_smallest_bar(sorted_by_seats):
 
 
 @load_data
-def get_closest_bar(sorted_by_seats, longitude, latitude):
-    my_coord = [[longitude, latitude]]
-    coords_list = []
-    for bar in sorted_by_seats:
-        coords_list.append(math.sqrt((my_coord[0][0] -
-                                      bar['geoData']['coordinates'][0]) ** 2 +
-                                     ((my_coord[0][1] -
-                                      bar['geoData']['coordinates'][1]) ** 2)))
-        coords_list.append(bar['Name'])
-        coords_list.append(bar['Address'])
-    nearest_bar_coord = min(coords_list[::3])
-    positiion_in_bar_coord_list = coords_list.index(nearest_bar_coord)
-    print("%s is closest to you. Address: %s" % (
-        coords_list.pop(positiion_in_bar_coord_list + 1),
-        coords_list.pop(positiion_in_bar_coord_list + 1)))
+def get_closest_bar(sorted_by_seats):
+    try:
+        longitude = input('Please enter Yours longitude: ')
+        latitude = input('Please enter Yours latitude: ')
+        my_coord = [[float(longitude), float(latitude)]]
+    except ValueError:
+        print('Incorrect longitude and latitude! Enter correct values.')
+    else:
+        SHIFT_ON_LIST = 1
+        coords_list = []
+        for bar in sorted_by_seats:
+            diff_of_coords = math.sqrt((my_coord[0][0] -
+                                        bar['geoData']['coordinates'][0])**2 +
+                                       ((my_coord[0][1] -
+                                         bar['geoData']['coordinates'][1])**2))
+            coords_list.append(diff_of_coords)
+            coords_list.append(bar['Name'])
+            coords_list.append(bar['Address'])
+        nearest_bar_coord = min(coords_list[::3])
+        positiion_in_bar_coord_list = coords_list.index(nearest_bar_coord)
+        print("%s is closest to you. Address: %s" % (
+            coords_list.pop(positiion_in_bar_coord_list + SHIFT_ON_LIST),
+            coords_list.pop(positiion_in_bar_coord_list + SHIFT_ON_LIST)))
 
 
 if __name__ == '__main__':
-    select_action_to_do()
+    input_path = input('Please enter way to JSON file: ')
+    if os.path.exists(input_path):
+        select_action = input('Please select what you want to do: '
+                              '\n 1)Find smallest bar. '
+                              '\n 2)Find biggest bar. '
+                              '\n 3)Find closest bar. ')
+        if select_action == '1':
+            get_smallest_bar(input_path)
+        elif select_action == '2':
+            get_biggest_bar(input_path)
+        elif select_action == '3':
+            try:
+                get_closest_bar(input_path)
+            except ValueError:
+                print('Incorrect longitude and latitude! '
+                      'Enter correct values.')
+        else:
+            print("Error! Type correct variant!")
+    else:
+        print('Incorrect directory! Check way!')
