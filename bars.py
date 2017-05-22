7ef9fb1f21398388f4a9558d542ecc799bc4a790
Ranc58 <rancvova@gmail.com>
2017-05-22 14:49:45 +0300

    refactor bars.py for correct functions and support_freindly view

--- bars.py
+++ bars.py
@@ -3,58 +3,60 @@ import math
 
 
 def load_data_output_content(filepath):
-    try:
-        with open(filepath, 'r', encoding='windows-1251') as file_handler:
-            return json.load(file_handler)
-    except (ValueError, FileNotFoundError) as error:
-        print(error, '\nIncorrect way or JSON file! Check it.')
-
-
-def get_biggest_bar(content):
-    biggest_bar = max(content, key=lambda seats: seats['SeatsCount'])
-    print('%s has %s seats in saloon.\n' %
-          (biggest_bar['Name'], biggest_bar['SeatsCount']))
+    with open(filepath, 'r', encoding='windows-1251') as file_handler:
+        return json.load(file_handler)
 
 
 def get_smallest_bar(content):
     smallest_bar = min(content, key=lambda seats: seats['SeatsCount'])
-    print('%s has %s seats in saloon.\n' %
+    return smallest_bar
+
+
+def get_biggest_bar(content):
+    biggest_bar = max(content, key=lambda seats: seats['SeatsCount'])
+    return biggest_bar
+
+
+def get_closest_bar(content, longitude, latitude):
+    for bar in content:
+        diff_of_coords = math.sqrt((longitude -
+                                    bar['geoData']['coordinates'][0])**2 +
+                                   ((latitude -
+                                     bar['geoData']['coordinates'][1])**2))
+        bar['geoData']['coordinates'] = diff_of_coords
+    closest_bar = min(content,
+                      key=lambda coords: coords['geoData']['coordinates'])
+    return closest_bar
+
+
+def print_smallest_bar(smallest_bar):
+    print('Smallest bar: \n%s has %s seats in saloon.\n' %
           (smallest_bar['Name'], smallest_bar['SeatsCount']))
 
 
-def closest_bar(content, longitude, latitude):
-    try:
-        my_coord = [[float(longitude), float(latitude)]]
-    except ValueError:
-        print('Error! Incorrect longitude and latitude! Enter correct values.')
-    else:
-        for bar in content:
-            diff_of_coords = math.sqrt((my_coord[0][0] -
-                                        bar['geoData']['coordinates'][0])**2 +
-                                       ((my_coord[0][1] -
-                                         bar['geoData']['coordinates'][1])**2))
-            bar['geoData']['coordinates'] = diff_of_coords
-        closest_bar = min(content, key=lambda coords:
-                          coords['geoData']['coordinates'])
-        return closest_bar
+def print_biggest_bar(biggest_bar):
+    print('Biggest bar: \n%s has %s seats in saloon.\n' %
+          (biggest_bar['Name'], biggest_bar['SeatsCount']))
 
 
-def get_closest_bar(closest_bar):
+def print_closest_bar(closest_bar):
     print("%s is closest to you. Address: %s" %
           (closest_bar['Name'], closest_bar['Address']))
 
 
 if __name__ == '__main__':
     input_path = input('Please enter way to JSON file: ')
-    content = load_data_output_content(input_path)
-    if content:
+    try:
+        content = load_data_output_content(input_path)
         longitude = input("Please enter longitude: ")
         latitude = input('Please enter latitude: ')
-        print('Biggest bar: ')
-        get_biggest_bar(content)
-        print('Smallest bar: ')
-        get_smallest_bar(content)
-        print('Closest bar: ')
-        closest_bar = closest_bar(content, longitude, latitude)
-        if closest_bar:
-            get_closest_bar(closest_bar)
+        smallest_bar = get_smallest_bar(content)
+        print_smallest_bar(smallest_bar)
+        biggest_bar = get_biggest_bar(content)
+        print_biggest_bar(biggest_bar)
+        print('Closest_bar: ')
+        closest_bar = get_closest_bar(content,
+                                      float(longitude), float(latitude))
+        print_closest_bar(closest_bar)
+    except (ValueError, FileNotFoundError):
+        print('Error! Check input information and JSON file for correctness.')
